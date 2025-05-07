81f82342a258087fcc172e0ad29a3320bd17c446
MamtaPatil <mamtapatil5252@gmail.com>
2025-05-07 17:40:46 +0530

    update

--- visa/visa-checklist-generator/src/app/checklist/page.js
+++ visa/visa-checklist-generator/src/app/checklist/page.js
@@ -1,23 +1,23 @@
-// 'use client';
-// import { useSearchParams } from 'next/navigation';
-// import { generateChecklist } from '../../utils/checklistLogic';
-// import ChecklistResult from '../../components/ChecklistResult';
-
-// export default function ChecklistPage() {
-//   const searchParams = useSearchParams();
-//   const form = Object.fromEntries(searchParams.entries());
-//   const checklist = generateChecklist(form);
-
-//   return (
-//     <ChecklistResult visaType={form.visaType} checklist={checklist} />
-//   );
-// }
-
-import ChecklistForm from '../../components/ChecklistForm';
-
-// Force dynamic rendering to skip prerendering
-export const dynamic = 'force-dynamic';
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { generateChecklist } from '../../utils/checklistLogic';
+import ChecklistResult from '../../components/ChecklistResult';
 
 export default function ChecklistPage() {
-  return <ChecklistForm />;
+  const searchParams = useSearchParams();
+  const form = Object.fromEntries(searchParams.entries());
+  const checklist = generateChecklist(form);
+
+  return (
+    <ChecklistResult visaType={form.visaType} checklist={checklist} />
+  );
 }
+
+// import ChecklistForm from '../../components/ChecklistForm';
+
+// // Force dynamic rendering to skip prerendering
+// export const dynamic = 'force-dynamic';
+
+// export default function ChecklistPage() {
+//   return <ChecklistForm />;
+// }
