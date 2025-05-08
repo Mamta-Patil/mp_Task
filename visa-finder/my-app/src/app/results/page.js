5bc2117cb6a82bac57619588c42ab46ddb2e18af
MamtaPatil <mamtapatil5252@gmail.com>
2025-05-08 12:21:13 +0530

    update

--- visa-finder/my-app/src/app/results/page.js
+++ visa-finder/my-app/src/app/results/page.js
@@ -9,6 +9,18 @@
 //   );
 // }
 
+// import { Suspense } from 'react';
+// import ResultsContent from '@/components/ResultsContent';
+
+// export default function ResultsPage() {
+//   return (
+//     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading visa suggestions...</div>}>
+//       <ResultsContent />
+//     </Suspense>
+//   );
+// }
+
+
 import { Suspense } from 'react';
 import ResultsContent from '@/components/ResultsContent';
 
